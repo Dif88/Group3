1f773643f6b68e4042b3c953fb2d9e46b1bff994
Dif88 <117322690+Dif88@users.noreply.github.com>
2026-04-24 12:21:03 -0400

    change

---
+++ Assets/BTM_Assets/BTM_Items_Gems/Scripts/PlayerInventory.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+      public int NumberOfStars {get; private set;}
+
+      public void StarsCollected()
+      {
+        NumberOfStars++;
+      }
+
+}
